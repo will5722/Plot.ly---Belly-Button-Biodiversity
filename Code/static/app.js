8e0f01316a1d25553c0f7b5cae3936dafd8d9a8d
Willam Lubenow <wkl22@humboldt.edu>
2021-11-12 11:20:07 -0800

    Demographics table added

--- Code/static/app.js
+++ Code/static/app.js
@@ -1,17 +1,44 @@
 //Function to create initial graphs and populate the dropdown menu.
 function init() {
-    d3.json("../Data/samples.json").then((sampleData => {
+    d3.json("../Data/samples.json").then((sampleData) => {
         //Select the dropdown menu.
         var menu = d3.select("#selDataset");
-        
-        sampleData.names.forEach((name) => {
-            menu.append("option").text(name);
+        var names = sampleData.names;
+        names.forEach((name) => {
+            menu.append("option").text(name).property("value", name);
         });
 
-        //TO DO: create/add function to create initial plots 
-    }
-    ))
+        //Create/add function to create initial plots 
+        var initSample = sampleData.names[0]
+        createGraphs(initSample);
+    })
 };
+
+
+function createGraphs(names) {
+    d3.json("../Data/samples.json").then((sampleData) => {
+        //Variable for metadata, select demographic table
+        var metadata = sampleData.metadata;
+        var demoTable = d3.select("#sample-metadata");
+
+        //Clear data in the table
+        demoTable.html("");
+
+        //Variable for each samples metadata
+        var singleMetadata = metadata.filter(sample => sample.id == names)[0];
+
+        //Add the data to the table
+        Object.entries(singleMetadata).forEach(([key, value]) => {
+            demoTable.append("h5").text(`${key}: ${value}`);
+        });
+    })
+};
+
+
+function optionChanged(differentSample) {
+    createGraphs(differentSample);
+};
+
 
 
 init();
